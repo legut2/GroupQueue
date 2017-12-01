fc47bfaf9c18eb68b7760b1780fec374fbd89410
NoSegfault <jerrynjzhang@gmail.com>
2017-11-30 21:49:15 -0600

    enable pause in host view

--- app/src/main/java/com/example/groupqueueprototype/android_studio_project_folder/HostViewActivity.java
+++ app/src/main/java/com/example/groupqueueprototype/android_studio_project_folder/HostViewActivity.java
@@ -3,17 +3,31 @@ package com.example.groupqueueprototype.android_studio_project_folder;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HostViewActivity extends Activity {
 
-    MediaPlayer mediaPlayer;
+    MediaPlayer musicPlayer;
+    private Button playSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_view);
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.walk_on_water);
+        musicPlayer = MediaPlayer.create(getApplicationContext(), R.raw.walk_on_water);
 
-        mediaPlayer.start();
+        playSong = (Button) findViewById (R.id.play_btn);
+
+        playSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(musicPlayer.isPlaying()){
+                    musicPlayer.pause();
+                } else {
+                    musicPlayer.start();
+                }
+            }
+        });
     }
 }
