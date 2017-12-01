9fffd3e0baf2be7fcd0765142873c7ec624629c6
NoSegfault <jerrynjzhang@gmail.com>
2017-11-30 21:40:02 -0600

    modified voting stage

--- app/src/main/java/com/example/groupqueueprototype/android_studio_project_folder/VoteActivity.java
+++ app/src/main/java/com/example/groupqueueprototype/android_studio_project_folder/VoteActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class VoteActivity extends Activity {
 
@@ -11,6 +16,21 @@ public class VoteActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+        String [] s = new String[] {"Vote on this", "Vote on this", "Vote on this",
+                "Vote on this", "Vote on this", "Vote on this", "Vote on this", "Vote on this"};
+        ListView lv = (ListView)findViewById(R.id.songList);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, s));
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                ListView lv = (ListView) arg0;
+                TextView tv = (TextView) lv.getChildAt(arg2);
+                String s = tv.getText().toString();
+                Toast.makeText(VoteActivity.this, "Clicked item is "+s, Toast.LENGTH_LONG).show();
+            } });
     }
 
 
